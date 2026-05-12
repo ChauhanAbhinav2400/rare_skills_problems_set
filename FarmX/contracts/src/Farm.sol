edbb2e91a90139e00ac0e110c0516a9ecf685fb0
ChauhanAbhinav2400 <145866171+ChauhanAbhinav2400@users.noreply.github.com>
2026-05-12 11:26:00 +0000

    completed for single pool

--- FarmX/contracts/src/Farm.sol
+++ FarmX/contracts/src/Farm.sol
@@ -1,12 +1,16 @@
 
+// SPDX-License-Identifier : MIT
+pragma solidity ^0.8.26;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {RewardToken} from "./RewardToken.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-
-contract Farm {
+contract Farm is ReentrancyGuard {
 
 using SafeERC20 for IERC20;
+using SafeERC20 for RewardToken;
 
 error ZeroAmount();
 error InvalidAddress();
@@ -16,7 +20,7 @@ event Deposit(address indexed user , uint256 amount );
 event Withdraw(address indexed user , uint256 amount);
 
 IERC20 public lpToken;
-IERC20 public rewardToken;
+RewardToken public rewardToken;
 
 struct UserInfo {
     uint256 amount;
@@ -33,37 +37,85 @@ mapping(address => UserInfo) public userInfo;
 constructor(address _lpToken , address _rewardToken) {
     if(_lpToken == address(0) || _rewardToken == address(0)) revert InvalidAddress();
     lpToken = IERC20(_lpToken);
-    rewardToken = IERC20(_rewardToken);
+    rewardToken = RewardToken(_rewardToken);
+    lastRewardTime = uint40(block.timestamp);
+    rewardPerSecond = 1e18; 
 }
 
 
-function deposit(uint256 amount) external {
-    
-    if (amount == 0 ) revert ZeroAmount();
+function deposit(uint256 amount) external nonReentrant {
+
+
     _updatePool();
-    UserInfo memory user = userInfo[msg.sender];
+    UserInfo storage user = userInfo[msg.sender];
     uint256 pendingRewards;
     if(user.amount > 0 ){
-     pendingRewards = ((user.amount * accRewardPerShare ) / WAD) - user.rewardDebt;
+        if(((user.amount * accRewardPerShare) / WAD) < user.rewardDebt) {
+            pendingRewards = 0;
+        } else {
+            pendingRewards = _pendingRewards(user, accRewardPerShare);
+        }
      rewardToken.mint(msg.sender,pendingRewards);
     } 
     lpToken.safeTransferFrom(msg.sender,address(this),amount);
     user.amount += amount;
     totalStaked += amount;
-    lastRewardTime = uint40(block.timestamp);
+    user.rewardDebt = (user.amount * accRewardPerShare) / WAD;
+
 
    emit Deposit(msg.sender, amount);
 
 }
 
 
-function withdraw(uint256 amount) external {
-    if (amount <= 0 ) revert ZeroAmount();
-    if (balance[msg.sender] < amount) revert InsufficientBalance();
-   
-    totalStaked -= amount;
-    lpToken.safeTransfer(msg.sender , amount);
+
+
+function withdraw(uint256 amount) external nonReentrant {
+    if (amount == 0 ) revert ZeroAmount();
+     _updatePool();
+     UserInfo storage user = userInfo[msg.sender];
+     if(user.amount < amount) revert InsufficientBalance();
+      uint256 pendingRewards;
+      if(((user.amount * accRewardPerShare) / WAD) < user.rewardDebt) {
+            pendingRewards = 0;
+        } else {
+            pendingRewards = _pendingRewards(user , accRewardPerShare);
+        }
+     rewardToken.mint(msg.sender,pendingRewards);
+     user.amount -= amount;
+     totalStaked -= amount;
+     lpToken.safeTransfer(msg.sender,amount);
+     user.rewardDebt = (user.amount * accRewardPerShare) / WAD;
     emit Withdraw(msg.sender, amount);
+}
+
+
+function _pendingRewards(UserInfo memory _user , uint256 accRPS) internal view returns( uint256 ) {
+    return  ((_user.amount * accRPS) / WAD) - _user.rewardDebt;     
+}
+
+function viewRewards(address _user) external view returns(uint256) {
+     
+         UserInfo memory user = userInfo[_user];
+         uint256 accumulatedRewardPerShare = accRewardPerShare;
+         if(block.timestamp > lastRewardTime && totalStaked != 0){
+           accumulatedRewardPerShare +=  increaseInRewardPerShare();
+         }
+    return _pendingRewards(user, accumulatedRewardPerShare);
+}
+
+function userStakedAmount(address _user) external view returns(uint256) {
+    return userInfo[_user].amount;
+}
+
+function tvl() external view returns(uint256 ){
+    return totalStaked;
+}
+
+function increaseInRewardPerShare() internal view returns(uint256) {
+    uint256 timeElapsed = block.timestamp - lastRewardTime;
+    uint256 reward = timeElapsed * rewardPerSecond;
+    return (reward * WAD) / totalStaked;
 }
 
 function _updatePool() internal {
@@ -74,12 +126,10 @@ function _updatePool() internal {
         lastRewardTime = uint40(block.timestamp);
         return;
     }
-
-    uint256 timeElasped = block.timestamp - lastRewardTime;
-    uint256 reward = timeElasped * rewardPerSecond;
-    accRewardPerShare += reward * WAD / totalStaked;
+    accRewardPerShare += increaseInRewardPerShare();
     lastRewardTime = uint40(block.timestamp);
 }
+
 
 
 }
